17b2d523996506e3860a60af1a5987dd91851fec
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2018-01-23 11:53:24 +0200

    Adds color config
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- app/config.js
+++ app/config.js
@@ -4,12 +4,20 @@ angular.module('config', [])
   .constant('config', {
     'endpoint'    : "http://"+window.location.hostname+":8080",
     'provider'    : "itsyouonline", // google, github, gitlab, keycloak, saml2 or basic
-    'client_id'   : "cockpitorg1",
+    'client_id'   : "<client_id>",
     'github_url'  : null,  // replace with your enterprise github server
     'gitlab_url'  : "https://gitlab.com",  // replace with your gitlab server
     'keycloak_url': "https://keycloak.example.org",  // replace with your keycloak server
     'keycloak_realm': "master",  // replace with your keycloak realm
-    'colors'      : {}, // use default colors
+    'colors'      : {
+      'severity': {
+        'ERROR'        : '#B22222',
+        'WARNING'      : '#FF7F50',
+        'OK'           : '#00AA5A',
+        'unknown'      : '#A6ACA8'
+      },
+      'highlight': 'lightgray'
+    },
     'severity'    : {}, // use default severity codes
     'audio'       : {}, // no audio
     'tracking_id' : ""  // Google Analytics tracking ID eg. UA-NNNNNN-N
